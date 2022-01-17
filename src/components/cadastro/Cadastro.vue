4fca1bd948a33fbe9749a6a834e851e8aca5c895
AckermanFernando <fernandoloiola03@gmail.com>
2022-01-17 12:38:22 -0300

    card de cadatro

--- src/components/cadastro/Cadastro.vue
+++ src/components/cadastro/Cadastro.vue
@@ -1,41 +1,43 @@
-<template>
-  <div>
-    <h1 class="centralizado">Cadastro</h1>
-    <h2 class="centralizado">{{ foto.titulo }}</h2>
+<template class="conteudo">
+  <div class="corpo">
+    <div >
+      <h1 class="centralizado">Cadastro</h1>
+      <h2 class="centralizado">{{ foto.titulo }}</h2>
 
-    <form @submit.prevent="gravar()">
-      <div class="controle">
-        <label for="titulo">TÍTULO</label>
-        <input id="titulo" autocomplete="off" v-model.lazy="foto.titulo" />
-      </div>
+      <form @submit.prevent="gravar()">
+        <div class="controle">
+          <label for="titulo">TÍTULO</label>
+          <input id="titulo" autocomplete="off" v-model.lazy="foto.titulo" />
+        </div>
 
-      <div class="controle">
-        <label for="url">URL</label>
-        <input id="url" autocomplete="off" v-model.lazy="foto.url" />
-        <imagem-responsiva
-          v-show="foto.url"
-          :url="foto.url"
-          :titulo="foto.titulo"
-        />
-      </div>
+        <div class="controle">
+          <label for="url">URL</label>
+          <input id="url" autocomplete="off" v-model.lazy="foto.url" />
+          <imagem-responsiva
+            v-show="foto.url"
+            :url="foto.url"
+            :titulo="foto.titulo"
+          />
+        </div>
 
-      <div class="controle">
-        <label for="descricao">DESCRIÇÃO</label>
-        <textarea
-          id="descricao"
-          autocomplete="off"
-          v-model="foto.descricao"
-        ></textarea>
-        <!--v-model é igual a @input="foto.descricao = $event.target.value" :value="foto.descricao">   -->
-      </div>
+        <div class="controle">
+          <label for="descricao">DESCRIÇÃO</label>
+          <textarea
+            id="descricao"
+            autocomplete="off"
+            v-model="foto.descricao"
+          ></textarea>
+          <!--v-model é igual a @input="foto.descricao = $event.target.value" :value="foto.descricao">   -->
+        </div>
 
-      <div class="centralizado">
-        <meu-botao rotulo="GRAVAR" tipo="submit" />
-        <router-link to="/"
-          ><meu-botao rotulo="VOLTAR" tipo="button"
-        /></router-link>
-      </div>
-    </form>
+        <div class="centralizado">
+          <meu-botao rotulo="GRAVAR" tipo="submit" />
+          <router-link to="/"
+            ><meu-botao rotulo="VOLTAR" tipo="button"
+          /></router-link>
+        </div>
+      </form>
+    </div>
   </div>
 </template>
 
@@ -67,13 +69,23 @@ export default {
 };
 </script>
 <style scoped>
-form{
-  padding: 2rem 14rem;
+.conteudo{
+  display: flex;
+  justify-content: center;
+}
+.corpo {
+  display: flex;
+  justify-content: center;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.6);
+  -webkit-box-shadow: 0 0 10px rgba(0, 0, 0, 0.6);
+  width: 65%;
+}
+form {
+  padding: 2rem 3rem;
 }
 h1.centralizado {
   color: #0e0f33;
-  text-shadow: -1px 0 black, 0 1px black,
-      1px 0 black, 0 -1px black ;
+  text-shadow: -1px 0 black, 0 1px black, 1px 0 black, 0 -1px black;
 }
 .controle {
   font-size: 1.2em;
