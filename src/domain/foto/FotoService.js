7f4a150158ffb56bba28bf27aef28eceefe1bd1d
AckermanFernando <fernandoloiola03@gmail.com>
2022-01-19 11:34:23 -0300

    implementando retorno da mensagem de retorno

--- src/domain/foto/FotoService.js
+++ src/domain/foto/FotoService.js
@@ -4,14 +4,35 @@ export default class FotoService {
   }
 
   lista () {
-    return this._resource.query().then(res => res.json())
+    return this._resource.query().then(
+      res => res.json(),
+      err => {
+        console.log(err)
+        throw new Error(
+          'Não foi possível exibir as imagens. Tente novamente mais tarde!'
+        )
+      }
+    )
   }
 
   cadastra (foto) {
-    return this._resource.save(foto)
+    if (foto._id) {
+      return this._resource.update({ id: foto._id }, foto)
+    } else {
+      return this._resource.save(foto)
+    }
   }
 
   apaga (id) {
-    return this._resource.delete({id})
+    return this._resource
+      .delete({ id })
+      .then(null, err => {
+        console.log(err)
+        throw new Error('Não foi possível apagar a imagem')
+      })
+  }
+
+  busca (id) {
+    return this._resource.get({ id }).then(res => res.json())
   }
 }
